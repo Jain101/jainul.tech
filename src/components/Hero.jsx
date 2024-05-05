5da57cf3a0eeae80c9f3e63b678dbb3321cf944f
Jain101 <2021it1ja@mitsgwl.ac.in>
2024-05-06 00:03:02 +0530

    prod issue

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,14 +11,13 @@ export default function Hero() {
                     </div> */}
                     <div className="avatar">
                         <div className="w-32 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
-                            <img src="/ash2.gif" />
+                            <img src="/ash2.gif" alt="ash" />
                         </div>
                     </div>
-
                     <h1 className="text-5xl font-bold">Hi 🙋‍♂️, I am Jainul</h1>
                     <div className="py-6">
-                        <p className='text-center'>An aspiring Software Developer navigating the software horizon by building some cool stuffs. For me building stuff is like training my pokemons(skills).</p>
-                        <p className='text-center'>I am just trying to make my journey as adventurous as that of Ash. Doesn't it sound cool?</p>
+                        <p className="text-center">An aspiring Software Developer navigating the software horizon by building some cool stuffs. For me building stuff is like training my pokemons(skills).</p>
+                        <p className="text-center">I am just trying to make my journey as adventurous as that of Ash. Doesn't it sound cool?</p>
                     </div>
                     <Link className="btn btn-primary" href={"https://drive.google.com/file/d/1wQ-1VllpkPG0VrrVikI8advDyVmh1yFI/view?usp=drive_link"}>Resume</Link>
                 </div>
